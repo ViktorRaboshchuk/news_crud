1817956f9bdf5b282cd142623581731ab3a44721
Viktor <viktor.raboshchuk@gmail.com>
2021-04-02 09:41:38 +0300

    commit message

--- news/news/settings.py
+++ news/news/settings.py
@@ -86,32 +86,32 @@ WSGI_APPLICATION = 'news.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-
     'default': {
-
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-        'NAME': 'chat_room',
-
-        'USER': 'postgres',
-
-        'PASSWORD': '123',
-
-        'HOST': 'localhost',
-
-        'PORT': '5432',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-
 }
+
+# DATABASES = {
+#
+#     'default': {
+#
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#
+#         'NAME': 'chat_room',
+#
+#         'USER': 'postgres',
+#
+#         'PASSWORD': '123',
+#
+#         'HOST': 'localhost',
+#
+#         'PORT': '5432',
+#
+#     }
+#
+# }
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
