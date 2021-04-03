0913fedd9addc17e76eaaee77e48f7e52a977bca
Viktor <viktor.raboshchuk@gmail.com>
2021-04-03 21:10:13 +0300

    heroku conf

--- news/crud/votes_scheduler.py
+++ news/crud/votes_scheduler.py
@@ -5,5 +5,5 @@ def start():
     scheduler = BackgroundScheduler()
     from crud.views import VotesDeleteView
     vote_up = VotesDeleteView()
-    scheduler.add_job(vote_up.delete, 'interval', seconds=20, id="delete_votes_001", replace_existing=True)
+    scheduler.add_job(vote_up.delete, 'interval', days=1, id="delete_votes_001", replace_existing=True)
     scheduler.start()
