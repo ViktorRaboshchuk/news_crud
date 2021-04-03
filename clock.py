a4f984415b206df8dd36a9da06e697b398662d7d
Viktor <viktor.raboshchuk@gmail.com>
2021-04-03 15:00:55 +0300

    heroku conf

--- clock.py
+++ clock.py
@@ -3,15 +3,6 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 scheduler = BackgroundScheduler()
 
-
-@scheduler.scheduled_job('interval', minutes=0.5)
-def timed_job():
-    print('This job is run every three minutes.')
-
-
-# @scheduler.scheduled_job('cron', hour=24)
-# def scheduled_job():
-#     print('This job is run every weekday at 5pm.')
-
+scheduler.add_job(lambda: scheduler.print_jobs(), 'interval', seconds=5)
 
 scheduler.start()
